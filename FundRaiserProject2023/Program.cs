da6d9dfcf574cf3947509e306945037c818360f4
Angelos Garinis <garinis_ang@hotmail.com>
2023-07-12 14:23:51 +0300

    Connection to Local Database

--- FundRaiserProject2023/Program.cs
+++ FundRaiserProject2023/Program.cs
@@ -1,10 +1,17 @@
-using FundRaiserProject2023.DbContexts;
+using FundRaiserProject2023.Services;
+using Microsoft.Data.SqlClient;
 
+//connection to database
+var ConnectToDB = new ConnectToDB();
+ConnectToDB.Connect();
+
+
+//application start
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<OurDbContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- FundRaiserProject2023/Services/ConnectToDB.cs
+++ FundRaiserProject2023/Services/ConnectToDB.cs
@@ -9,23 +9,31 @@ namespace FundRaiserProject2023.Services
 {
     public class ConnectToDB
     {
-        static void Main(string[] args)
+        public void Connect()
         {
             Console.WriteLine("Getting Connection...");
             //Your Connection string here
-            string connString = @"Data Source=(local); Initial Catalog=FundRaiser-2023; Integrated security=true; TrustServerCertificate=True";
-            SqlConnection conn = new SqlConnection(connString);
-
+            string connectionString = "Data Source=(local); Initial Catalog=MyTestFundRaiserDB; Integrated security=true; TrustServerCertificate=True";
+            using var connection = new SqlConnection(connectionString);
             try
             {
-                conn.Open();
+                connection.Open();
 
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex.Message);
             }
-            ;
+
+            //local query example
+            string query = "SELECT Name FROM Users JOIN Backers ON Users.Id = Backers.Id JOIN Projects ON Backers.ProjectId = Projects.Id WHERE Title = 'Title1'";
+            using var command = new SqlCommand(query, connection);            
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"Name: {reader.GetString(0)}");
+            }
         }
     }
 }
