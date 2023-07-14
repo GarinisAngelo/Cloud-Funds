e4791d36451b4a3aea639e42d7c566d91462b3e5
Angelos Garinis <garinis_ang@hotmail.com>
2023-07-14 14:30:49 +0300

    Special WelcomePage for John

--- FundRaiserProject2023/Controllers/HomeController.cs
+++ FundRaiserProject2023/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace FundRaiserProject2023.Controllers
         {
             return View();
         }
+        public IActionResult WelcomePage()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
