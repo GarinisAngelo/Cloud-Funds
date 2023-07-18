dff14caac42bbf47d4fe0c8caf2d18ebf4b18406
iioannidis94 <i.ioannidis.dev@gmail.com>
2023-07-18 12:05:32 +0300

    css and js Update

--- FundRaiserProject2023/wwwroot/js/site.js
+++ FundRaiserProject2023/wwwroot/js/site.js
@@ -2,3 +2,49 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const scrollPositions = {
+    'multi-carousel1': 0,
+    'multi-carousel2': 0
+}
+
+function data(id) {
+    const multiCarousel = document.querySelector(`#${id}`);
+    const carouselInner = multiCarousel.querySelector('.carousel-inner');
+    const item = carouselInner.querySelector('.carousel-item');
+
+    const itemWidth = item.scrollWidth;
+    const carouselWidth = multiCarousel.scrollWidth;
+
+    return {
+        carouselInner: carouselInner,
+        itemWidth: itemWidth,
+        carouselWidth: carouselWidth
+    };
+}
+
+function scroll(carouselInner, distance) {
+    carouselInner.scroll({
+        left: distance,
+        top: 0,
+        behavior: "smooth",
+    });
+}
+
+
+function next(id) {
+    const { carouselInner, itemWidth, carouselWidth } = data(id);
+
+    if (scrollPositions[id] < carouselWidth) scrollPositions[id] += itemWidth;
+    else scrollPositions[id] = 0;
+
+    scroll(carouselInner, scrollPositions[id]);
+}
+
+function previous(id) {
+    const { carouselInner, itemWidth, carouselWidth } = data(id);
+
+    if (scrollPositions[id] > 0) scrollPositions[id] -= itemWidth;
+    else scrollPositions[id] = carouselWidth;
+
+    scroll(carouselInner, scrollPositions[id]);
+}
