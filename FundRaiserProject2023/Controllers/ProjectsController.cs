1d88cd6bdb0968cfe94d0e4c328ab9a40551ea4a
Angelos Garinis <garinis_ang@hotmail.com>
2023-07-18 15:19:53 +0300

    Readjusted

--- FundRaiserProject2023/Controllers/ProjectsController.cs
+++ FundRaiserProject2023/Controllers/ProjectsController.cs
@@ -63,59 +63,6 @@ namespace FundRaiserProject2023.Controllers
             return View(project);
         }
 
-        // GET: Projects/Edit/5
-        public async Task<IActionResult> UpdateDatabase(int? id)
-        {
-            if (id == null || _context.Projects == null)
-            {
-                return NotFound();
-            }
-
-            var project = await _context.Projects.FindAsync(id);
-            if (project == null)
-            {
-                return NotFound();
-            }
-            return View(project);
-        }
-
-        // POST: Projects/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> UpdateDatabase(int id, [Bind("Id,Title,Description,FundingGoal,CurrentFunding")] Project project, int rewardId)
-        {
-            if (id != project.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    var selectedReward = _context.RewardPackages.FindAsync(rewardId);
-                    project.CurrentFunding += rewardId;
-                    _context.Update(project.CurrentFunding);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProjectExists(project.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(project);
-        }
-
         // GET: Projects
         public async Task<IActionResult> Index()
         {
